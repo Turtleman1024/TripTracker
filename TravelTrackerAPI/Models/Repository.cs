8a01f901a5fc452418e24457db1f55761079094c
Turtleman1024 <kmurry6262@gmail.com>
2019-10-03 22:44:49 -0700

    First commit with basic API setup with Swagger

---
+++ TravelTrackerAPI/Models/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelTrackerAPI.Models
+{
+    public class Repository
+    {
+        private List<Trip> MyTrips = new List<Trip>
+        {
+            new Trip
+            {
+                Id = 1,
+                Name = "Disney Land 2019",
+                StartDate = new DateTime(2019, 10, 10),
+                EndDate = new DateTime(2019, 10, 15)
+            },
+            new Trip
+            {
+                Id = 2,
+                Name = "Universal Studio 2019",
+                StartDate = new DateTime(2019, 10, 16),
+                EndDate = new DateTime(2019, 10, 18)
+            },
+            new Trip
+            {
+                Id = 3,
+                Name = "Sea World 2019",
+                StartDate = new DateTime(2019, 10, 19),
+                EndDate = new DateTime(2019, 10, 20)
+            }
+        };
+
+        public List<Trip> Get()
+        {
+            return MyTrips;
+        }
+
+        public Trip Get(int id)
+        {
+            return MyTrips.FirstOrDefault(t => t.Id == id);
+        }
+
+        public void Add(Trip newTrip)
+        {
+            MyTrips.Add(newTrip);
+        }
+
+        public void Update (Trip tripToUpdate)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            Add(tripToUpdate);
+        }
+
+        public void Remove(int id)
+        {
+            MyTrips.Remove(MyTrips.First(t => t.Id == id));
+        }
+    }
+}
---
+++ TravelTrackerAPI/Models/Segment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelTrackerAPI.Models
+{
+    public class Segment
+    {
+        public int Id { get; set; }
+        public int TripId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset StateDateTime { get; set; }
+        public DateTimeOffset EndDateTime { get; set; }
+        
+    }
+}
