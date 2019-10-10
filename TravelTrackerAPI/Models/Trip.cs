e404bd0d0252126a4ff7cef59ef8f8c7d8ea3332
Turtleman1024 <kmurry6262@gmail.com>
2019-10-09 19:43:13 -0700

    Adding DataAnnotations

--- TravelTrackerAPI/Models/Trip.cs
+++ TravelTrackerAPI/Models/Trip.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,17 @@ namespace TravelTrackerAPI.Models
 {
     public class Trip
     {
+        //EntityFrameWork convention will use the prop Id as the primary key
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime EndDate { get; set; }
     }
 }
